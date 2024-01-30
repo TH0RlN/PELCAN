78aa8ee742975eacc3705e92fc3b000efa21ea30
TH0RlN <aprariasperez@gmail.com>
2024-01-30 11:30:32 +0100

    casi no

--- pelcan-django/mysite/polls/views.py
+++ pelcan-django/mysite/polls/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': questions}
+    return HttpResponse(render(request, 'polls/index.html', context))
 
-# Create your views here.
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404(render(request, 'polls/404.html'))
+    return HttpResponse(render(request, 'polls/detail.html', {'question': question}))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)

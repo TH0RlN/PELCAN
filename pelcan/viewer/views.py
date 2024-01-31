cc7730045e61ac58785dec45d90dfb6a83cd27f5
Raúl Arias Pérez <aprariasperez@gmail.com@gmail.com>
2024-01-31 23:16:37 +0100

    index view working

--- pelcan/viewer/views.py
+++ pelcan/viewer/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Cita
+import datetime
 
 def index(request):
-    return render(request, "viewer/index.html")
+    citas_hoy = Cita.objects.filter(fecha=datetime.date.today())
+    context = {"citas_hoy": citas_hoy}
+
+    return render(request, "viewer/index.html", context)
